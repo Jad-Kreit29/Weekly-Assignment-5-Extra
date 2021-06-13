e70a7b15c4479402ac5995c555a2f86959dfb83d
Jad Kreit <84517030+Jad-Kreit29@users.noreply.github.com>
2021-06-13 19:55:03 +0000

    Added system response to unexpected answers

--- main.py
+++ main.py
@@ -16,21 +16,26 @@ case = 1
 reask = 0
 
 while True:
-    case = int(input("Type '1' for area or type '2' for circumference: "))
-    if case == 1:
-      diameter = int(input("What's the diameter of the circle? " ))
-      print ("Diameter:", diameter)
-      radius = int(diameter / 2)
-      print("Radius is:", radius)
-      area = math.pi * radius ** 2
-      print("Area of the circle is:", area)
-    elif case == 2:
-      diameter = int(input("What's the diameter of the circle? "))
-      print("Diameter", diameter)
-      radius = int(diameter / 2)
-      print("Radius is:", radius)
-      circumference = 2 * math.pi * radius
-      print("Circumference of the circle is:", circumference)
+    try:
+      case = int(input("Type '1' for area or type '2' for circumference: "))
+      if case == 1:
+        diameter = int(input("What's the diameter of the circle? " ))
+        print("Diameter:", diameter)
+        radius = int(diameter / 2)
+        print("Radius:", radius)
+        area = math.pi * radius ** 2
+        print("Area of the circle is:", area)
+      elif case == 2:
+        diameter = int(input("What's the diameter of the circle? "))
+        print("Diameter:", diameter)
+        radius = int(diameter / 2)
+        print("Radius:", radius)
+        circumference = 2 * math.pi * radius
+        print("Circumference of the circle is:", circumference)
+      else:
+        print("Please input a real option")
+    except ValueError:
+      print("Please input a number!")
     while True:
         reask = str(input("New Calculation? (y/n): "))
         if reask in ("y", "n"):
