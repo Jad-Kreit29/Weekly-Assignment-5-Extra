29ec85f7b9f2b3679ef5cb066d02b9cb904e0314
Jad Kreit <84517030+Jad-Kreit29@users.noreply.github.com>
2021-06-14 16:56:15 +0000

    Corrected Linter errors partially

--- main.py
+++ main.py
@@ -3,12 +3,14 @@ from os import system, name
 from time import sleep
 
 def clear():
-  if name == 'nt':
-    _ = system('cls')
-  else:
-    _ = system('clear')
+    if name == 'nt':
+        _ = system('cls')
+    else:
+        _ = system('clear')
+
 
 import math
+
 diameter = 0
 circumference = 0
 area = 0
@@ -17,25 +19,25 @@ reask = 0
 
 while True:
     try:
-      case = int(input("Type '1' for area or type '2' for circumference: "))
-      if case == 1:
-        diameter = int(input("What's the diameter of the circle? " ))
-        print("Diameter:", diameter)
-        radius = int(diameter / 2)
-        print("Radius:", radius)
-        area = math.pi * radius ** 2
-        print("Area of the circle is:", area)
-      elif case == 2:
-        diameter = int(input("What's the diameter of the circle? "))
-        print("Diameter:", diameter)
-        radius = int(diameter / 2)
-        print("Radius:", radius)
-        circumference = 2 * math.pi * radius
-        print("Circumference of the circle is:", circumference)
-      else:
-        print("Please input a real option")
+        case = int(input("Type '1' for area or type '2' for circumference: "))
+        if case == 1:
+            diameter = int(input("What's the diameter of the circle? " ))
+            print("Diameter:", diameter)
+            radius = int(diameter / 2)
+            print("Radius:", radius)
+            area = math.pi * radius ** 2
+            print("Area of the circle is:", area)
+        elif case == 2:
+            diameter = int(input("What's the diameter of the circle? "))
+            print("Diameter:", diameter)
+            radius = int(diameter / 2)
+            print("Radius:", radius)
+            circumference = 2 * math.pi * radius
+            print("Circumference of the circle is:", circumference)
+        else:
+            print("Please input a real option")
     except ValueError:
-      print("Please input a number!")
+        print("Please input a number!")
     while True:
         reask = str(input("New Calculation? (y/n): "))
         if reask in ("y", "n"):
@@ -49,4 +51,3 @@ while True:
         sleep(0.5)
         print("Goodbye. Thanks for calculating!")
         break
- 
